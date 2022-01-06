6c10c07dcf7083d87aec30e81b60f5cddd38ff50
Mehmet Serdar Dogan <21060620@stu.omu.edu.tr>
2022-01-07 00:57:36 +0300

    gereksiz değişkenler kaldırıldı

--- pi_olcer.rb
+++ pi_olcer.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
+# Pi sayısının yaklaşık değerini bir dairenin çevre uzunluğu yardımıyla bulduğum bir girişim 
 
-iki_pi = Math::PI * 2
 print 'Nokta sayısını giriniz: '
 noktasayisi = gets.chomp.to_f
 
-x = Math.cos(iki_pi / noktasayisi).round(10)
-y = Math.sin(iki_pi / noktasayisi).round(10)
+x = Math.cos(Math::PI * 2 / noktasayisi)
+y = Math.sin(Math::PI * 2 / noktasayisi)
 puts "Koordinatlar => (#{x}, #{y})"
 
-x_kare = (x - 1.0) ** 2
+x_kare = (x - 1.0) ** 2 # Dairedeki ilk nokta olan (1,0) noktasına olan uzaklığın karesi bulunur 
 y_kare = y * y
 c = x_kare + y_kare
 
 puts "Bir kenarın karesi => #{c}"
 
-birkenar = Math.sqrt(c).round(10)
+birkenar = Math.sqrt(c)
 puts "Tek kenar uzunluğu => #{birkenar}"
 
-deneysel_pi = birkenar * noktasayisi / 2
+deneysel_pi = birkenar * noktasayisi / 2 # Daire çevresinin daire çapına bölümü pi'yi verir
 puts "Deneysel sonuç => #{deneysel_pi}"
